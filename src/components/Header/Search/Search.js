4811859560c3d95e72c4cc9689c1b4ffd242b9d3
Mark Carey <markallencarey@gmail.com>
2020-11-23 15:17:47 -0700

    finished project

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -3,18 +3,42 @@ import React, { Component } from 'react';
 import './Search.css';
 
 import SearchIcon from 'react-icons/lib/md/search';
+// import axios from 'axios';
 
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
+
+
 export default class Search extends Component {
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     userInput: '',
+  //   }
+  // }
+
+  // handleSearchPosts(e) {
+  //   this.setState({
+  //     userInput: e.target.value,
+  //   })
+  // }
+  
+  // searchPosts(e){
+  //   e && e.preventDefault()
+
+  //   axios.get(`https://practiceapi.devmountain.com/api/posts/filter?text=${this.state.userInput}`).then(res => {
+  //     console.log(res.data)
+  //   })
+  // }
+
   render() {
     return (
       <section className="Search__parent">
 
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
+          <input placeholder="Search Your Feed" onChange={this.handleSearchPosts}/>
 
-          <SearchIcon id="Search__icon" />
+          <SearchIcon id="Search__icon" onClick={this.searchPosts}/>
         </div>
         
       </section>
